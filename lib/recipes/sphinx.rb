39a6bc30b4b5789236bb31ee437ae292d107e2e6
William Melody <wmelody@gmail.com>
2009-12-20 16:58:02 -0600

    typo in sphinx tasks

--- lib/recipes/sphinx.rb
+++ lib/recipes/sphinx.rb
@@ -6,13 +6,13 @@ namespace :thinking_sphinx do
   
   desc "Run the ThinkingSphinx indexer"
   task :index, :roles => :app do
-    run "cd #{release_path} && rake AILS_ENV=#{rails.env.to_s} thinking_sphinx:index"
+    run "cd #{release_path} && rake RAILS_ENV=#{rails.env.to_s} thinking_sphinx:index"
   end
   
   [ :stop, :start, :restart ].each do |t|
     desc "#{t.to_s.capitalize} the ThinkingSphinx daemon"
     task t, :roles => :app do
-      run "cd #{release_path} && rake AILS_ENV=#{rails.env.to_s} thinking_sphinx:index"
+      run "cd #{release_path} && rake RAILS_ENV=#{rails.env.to_s} thinking_sphinx:index"
     end
   end
 end
